c5dbedfd0f1abe40740c1bc4e890670f123aa79e
Martin Storsjö <martin@martin.st>
2025-06-10 19:14:18 +0300

    WIP: unittest: Tolerate the unexpected output of std::filesystem in libc++ before 20.x
    This was fixed in
    https://github.com/llvm/llvm-project/commit/aa427b1aae445ed46d9f60c5e2eaac61bdf76be3
    for libc++ 20.x.

--- unittest/test_util_path.cpp
+++ unittest/test_util_path.cpp
@@ -103,7 +103,10 @@ TEST_CASE("util::make_relative_path")
   SUBCASE("Path matches neither actual nor apparent CWD")
   {
 #ifdef _WIN32
-    CHECK(make_relative_path("C:/a", "C:/b", "C:/x") == "C:/x");
+    // libc++ before 20.x on Windows produces \x instead of C:/x here.
+    fs::path rel = make_relative_path("C:/a", "C:/b", "C:/x");
+    bool expected = rel == "C:/x" || rel == "\\x";
+    CHECK(expected);
 #else
     CHECK(make_relative_path("/a", "/b", "/x") == "/x");
 #endif
